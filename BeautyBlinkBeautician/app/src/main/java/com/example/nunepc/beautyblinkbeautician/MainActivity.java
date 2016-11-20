7aa64231ef2c91469493de0ba9cdbdbf7a002868
Unknown <sosotidaporn@gmail.com>
2016-11-21 02:46:29 +0800

    MainPage
    Create main page of beautician and create fragment of each button

--- BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/MainActivity.java
+++ BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.nunepc.beautyblinkbeautician;
 
 import android.content.res.Configuration;
+import android.media.Image;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.nunepc.beautyblinkbeautician.fragment.GalleryFragment;
+import com.example.nunepc.beautyblinkbeautician.fragment.NotiFragment;
+import com.example.nunepc.beautyblinkbeautician.fragment.PlannerFragment;
+import com.example.nunepc.beautyblinkbeautician.fragment.ProFragment;
+import com.example.nunepc.beautyblinkbeautician.fragment.RequestFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -18,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fragment
+        if(savedInstanceState==null){
+            //first create
+            //Place fragment
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentcontainer,new GalleryFragment())
+                    .commit();
+        }
 
         initInstances();
     }
@@ -38,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //tab button
+        findViewById(R.id.bt_gallery).setOnClickListener(this);
+        findViewById(R.id.bt_request).setOnClickListener(this);
+        findViewById(R.id.bt_planner).setOnClickListener(this);
+        findViewById(R.id.bt_noti).setOnClickListener(this);
+        findViewById(R.id.bt_promote).setOnClickListener(this);
+
     }
 
     @Override
@@ -53,4 +78,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_gallery:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentcontainer,GalleryFragment.newInstance())
+                        .commit();
+                break;
+            case R.id.bt_request:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentcontainer,RequestFragment.newInstance())
+                        .commit();
+                break;
+            case R.id.bt_planner:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentcontainer,PlannerFragment.newInstance())
+                        .commit();
+                break;
+            case R.id.bt_noti:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentcontainer,NotiFragment.newInstance())
+                        .commit();
+                break;
+            case R.id.bt_promote:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentcontainer,ProFragment.newInstance())
+                        .commit();
+                break;
+        }
+    }
 }
