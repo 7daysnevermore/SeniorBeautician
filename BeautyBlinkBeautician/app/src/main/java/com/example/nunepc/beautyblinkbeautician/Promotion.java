e40ec65e49828f1d9130a094e9a66eea1055c057
Unknown <sosotidaporn@gmail.com>
2017-01-13 23:04:35 +0800

    Edit promotion

--- BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/Promotion.java
+++ BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/Promotion.java
@@ -4,21 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import org.parceler.Parcels;
 
 import com.example.nunepc.beautyblinkbeautician.model.DataPromotion;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -34,8 +41,8 @@ public class Promotion extends AppCompatActivity {
     private TextView postP;
     private List<DataPromotion> movieList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private PostPromotion pp;
     private FirebaseAuth mAuth;
+    private FirebaseUser mFirebaseUser;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference databaseReference;
     private LinearLayout create_pro;
@@ -48,8 +55,9 @@ public class Promotion extends AppCompatActivity {
         setContentView(R.layout.activity_promotion);
 
         mAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mAuth.getCurrentUser();
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("beautician-promotion"+"/"+mAuth.getCurrentUser().getUid().toString());
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("beautician-promotion"+"/"+mFirebaseUser.getUid().toString());
         //professor promotion feeds
         recyclerView =(RecyclerView)findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -64,17 +72,32 @@ public class Promotion extends AppCompatActivity {
                 (DataPromotion.class,R.layout.promotion_row,PromotionViewHolder.class,databaseReference) {
 
             @Override
-            protected void populateViewHolder(PromotionViewHolder viewHolder, DataPromotion model, int position) {
+            protected void populateViewHolder(PromotionViewHolder viewHolder, DataPromotion model, final int position) {
+
                 viewHolder.setImage(getApplicationContext(),model.getImage());
                 viewHolder.setPromotion(model.getPromotion());
                 viewHolder.setPrice(model.getPrice());
                 viewHolder.setSale(model.getSale());
+
+                viewHolder.mview.setOnClickListener(new View.OnClickListener() {
+                    private static final String TAG = "Promotion";
+
+                    @Override
+                    public void onClick(View view) {
+                        Log.w(TAG, "You clicked on "+position);
+                        //firebaseRecyclerAdapter.getRef(position).removeValue();
+                    }
+                });
 
             }
         };
 
         recyclerView.setAdapter(firebaseRecyclerAdapter);
         recyclerView.setLayoutManager(mLayoutManager);
+
+
+
+
 
         create_pro = (LinearLayout)findViewById(R.id.create_pro);
         create_pro.setOnClickListener(new View.OnClickListener() {
@@ -93,14 +116,51 @@ public class Promotion extends AppCompatActivity {
 
     }
 
-    public static class PromotionViewHolder extends RecyclerView.ViewHolder{
+    public static class PromotionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         View mview;
+        Context mContext;
+
+        private FirebaseAuth mAuth;
+        private FirebaseUser mFirebaseUser;
 
         public PromotionViewHolder(View itemView){
             super(itemView);
             mview=itemView;
+            mContext = itemView.getContext();
 
+            mAuth = FirebaseAuth.getInstance();
+            mFirebaseUser = mAuth.getCurrentUser();
+
+            itemView.setOnClickListener(PromotionViewHolder.this);
+        }
+
+        @Override
+        public void onClick(View view){
+
+            final ArrayList<DataPromotion> dataPromotion = new ArrayList<>();
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("beautician-promotion"+"/"+mFirebaseUser.getUid().toString());
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        dataPromotion.add(snapshot.getValue(DataPromotion.class));
+                    }
+
+                    int itemPosition = getLayoutPosition();
+
+                    Intent intent = new Intent(mContext, PromotionDetails.class);
+                    intent.putExtra("position", itemPosition + "");
+                    intent.putExtra("restaurants", Parcels.wrap(dataPromotion));
+
+                    mContext.startActivity(intent);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
         }
 
         public void setPromotion(String promotion){
