f350abc4163475857cda272c9d53dec6d9ff46af
Unknown <sosotidaporn@gmail.com>
2017-01-16 02:30:35 +0800

    Promotion done

--- BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/EditPromotion.java
+++ BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/EditPromotion.java
@@ -1,84 +1,62 @@
 package com.example.nunepc.beautyblinkbeautician;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.nunepc.beautyblinkbeautician.model.User;
+import com.example.nunepc.beautyblinkbeautician.fragment.SettingFragment;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class PostPromotion extends AppCompatActivity {
+/**
+ * Created by NunePC on 16/1/2560.
+ */
 
-    private int REQUEST_CAMERA =0,SELECT_FILE=1;
-    private Toolbar toolbar;
-    private ImageView addPicture;
-    private EditText input_promotion,input_price,input_sale,input_df_day,input_df_month,input_df_year,
-            input_dt_day,input_dt_month,input_dt_year,input_details;
-    private TextView input_name;
-    String username;
-    private ImageView btn_createpromotion;
-    private Integer[] mThumbsId;
-    private Uri imageUri = null;
+public class EditPromotion extends AppCompatActivity implements View.OnClickListener {
+
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
     private ProgressDialog progressDialog;
-    private StorageReference storageReference,filepath;
+    private StorageReference storageReference, filepath;
     private DatabaseReference databaseReference;
 
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseUser mFirebaseUser;
+    HashMap<String, Object> promotionValues;
+    private ImageView addPicture, editImage;
+    private EditText input_promotion, input_price, input_sale, input_df_day, input_df_month, input_df_year,
+            input_dt_day, input_dt_month, input_dt_year, input_details;
+
+    private Uri imageUri = null;
+
+    String[] dateF, dateT;
+
+    boolean change = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_promotion);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_editpromotion);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        promotionValues = (HashMap<String, Object>) getIntent().getExtras().getSerializable("promotion");
 
-        storageReference = FirebaseStorage.getInstance().getReference();
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("Promotion");
+        dateF = promotionValues.get("dateFrom").toString().split("/");
+        dateT = promotionValues.get("dateTo").toString().split("/");
 
-        progressDialog = new ProgressDialog(this);
-        addPicture = (ImageView) findViewById(R.id.addPic);
-        addPicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent,SELECT_FILE);
-            }
-        });
-
-        input_promotion= (EditText)findViewById(R.id.promotion);
+        input_promotion = (EditText) findViewById(R.id.promotion);
         input_price = (EditText) findViewById(R.id.price);
         input_sale = (EditText) findViewById(R.id.sale);
         input_df_day = (EditText) findViewById(R.id.dateFrom_day);
@@ -89,32 +67,60 @@ public class PostPromotion extends AppCompatActivity {
         input_dt_year = (EditText) findViewById(R.id.dateTo_year);
         input_details = (EditText) findViewById(R.id.details);
 
-        btn_createpromotion = (ImageView)findViewById(R.id.btn_createpromotion);
+        input_promotion.setText(promotionValues.get("promotion").toString());
+        input_price.setText(promotionValues.get("price").toString());
+        input_sale.setText(promotionValues.get("sale").toString());
+        input_df_day.setText(dateF[0]);
+        input_df_month.setText(dateF[1]);
+        input_df_year.setText(dateF[2]);
+        input_dt_day.setText(dateT[0]);
+        input_dt_month.setText(dateT[1]);
+        input_dt_year.setText(dateT[2]);
+        input_details.setText(promotionValues.get("details").toString());
 
+        addPicture = (ImageView) findViewById(R.id.addPic);
+        editImage = (ImageView) findViewById(R.id.editimage) ;
 
-        btn_createpromotion.setOnClickListener(new View.OnClickListener() {
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Promotion");
+
+        editImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startPost();
+                change = true;
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, SELECT_FILE);
             }
         });
 
-    }
-    @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data ){
-        super.onActivityResult(requestCode,resultCode,data);
+        Picasso.with(this).load(promotionValues.get("image").toString()).into(addPicture);
 
-        if(requestCode == SELECT_FILE && resultCode == RESULT_OK){
+        findViewById(R.id.btn_editpromotion).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_FILE && resultCode == RESULT_OK) {
             imageUri = data.getData();
             addPicture.setImageURI(imageUri);
-
         }
 
     }
-    private void startPost(){
 
-        progressDialog.setMessage("Posting...");
-        progressDialog.show();
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_editpromotion:
+                editPromotion();
+                break;
+        }
+    }
+
+    public void editPromotion() {
 
         final String promotion = input_promotion.getText().toString();
         final String price = input_price.getText().toString();
@@ -127,11 +133,12 @@ public class PostPromotion extends AppCompatActivity {
         final String dt_year = input_dt_year.getText().toString();
         final String details = input_details.getText().toString();
 
-        if(!TextUtils.isEmpty(promotion) && !TextUtils.isEmpty(price) &&
+
+        if (!TextUtils.isEmpty(promotion) && !TextUtils.isEmpty(price) &&
                 !TextUtils.isEmpty(sale) && !TextUtils.isEmpty(df_day) &&
                 !TextUtils.isEmpty(df_month) && !TextUtils.isEmpty(df_year) &&
                 !TextUtils.isEmpty(dt_day) && !TextUtils.isEmpty(dt_month) &&
-                !TextUtils.isEmpty(dt_year) && !TextUtils.isEmpty(details) && imageUri != null){
+                !TextUtils.isEmpty(dt_year) && !TextUtils.isEmpty(details) && imageUri != null) {
 
             filepath = storageReference.child("Promotion").child(imageUri.getLastPathSegment());
 
@@ -142,9 +149,7 @@ public class PostPromotion extends AppCompatActivity {
 
                     //create root of Promotion
                     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-                    DatabaseReference mPromotionRef = mRootRef.child("promotion");
 
-                    String key = mPromotionRef.push().getKey();
 
                     final HashMap<String, Object> PromotionValues = new HashMap<>();
                     PromotionValues.put("promotion", promotion);
@@ -154,27 +159,28 @@ public class PostPromotion extends AppCompatActivity {
                     PromotionValues.put("datefrom", df_day + "/" + df_month + "/" + df_year);
                     PromotionValues.put("dateto", dt_day + "/" + dt_month + "/" + dt_year);
                     PromotionValues.put("details", details);
-                    PromotionValues.put("uid", mFirebaseUser.getUid().toString());
-                    PromotionValues.put("name", username);
+                    PromotionValues.put("uid", promotionValues.get("uid"));
+                    PromotionValues.put("name", promotionValues.get("name"));
 
 
-                    Map<String,Object> childUpdate = new HashMap<>();
-                    childUpdate.put("/promotion/"+key, PromotionValues);
-                    childUpdate.put("/beautician-promotion/"+mFirebaseUser.getUid().toString()+"/"+key, PromotionValues);
+                    Map<String, Object> childUpdate = new HashMap<>();
+                    childUpdate.put("/promotion/" + promotionValues.get("key"), PromotionValues);
+                    childUpdate.put("/beautician-promotion/" + promotionValues.get("uid") + "/" + promotionValues.get("key"), PromotionValues);
 
                     mRootRef.updateChildren(childUpdate);
 
                     //create root of Beautician-Promotion
 
-                    progressDialog.dismiss();
 
 
-                    startActivity(new Intent(PostPromotion.this,Promotion.class));
+                    startActivity(new Intent(EditPromotion.this, Promotion.class));
                 }
             });
 
 
         }
     }
-    }
 
+
+
+}
--- BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/PromotionDetails.java
+++ BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/PromotionDetails.java
@@ -1,11 +1,16 @@
 package com.example.nunepc.beautyblinkbeautician;
 
 
+import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,46 +22,67 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 
-public class PromotionDetails extends AppCompatActivity {
+public class PromotionDetails extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth;
-    private FirebaseUser mFirebaseUser;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private DatabaseReference databaseReference;
-    private TextView techTopic,techDetail,techPrice,techSale,techDT,techDF ;
+    HashMap<String, Object> promotionValues;
+    private TextView proTopic,proPrice,proSale,proDF,proDT,proDetails ;
+    ImageView proImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showpromotion);
 
-        mAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mAuth.getCurrentUser();
-        String cshow = getIntent().getExtras().getString("to_show");
-       // Toast.makeText(PromotionDetails.this, cshow,Toast.LENGTH_LONG).show();
-        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference mPromotionRef = mRootRef.child("beautician-promotion"+"/"+mFirebaseUser.getUid().toString());
-        techTopic = (TextView)findViewById(R.id.proTopic);
-        mPromotionRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Map<String, String> map = (Map)dataSnapshot.getValue();
-                String mTop= map.get("promotion");
-                techTopic.setText(mTop);
-            }
+        promotionValues = (HashMap<String, Object>) getIntent().getExtras().getSerializable("promotion");
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+        proTopic = (TextView)findViewById(R.id.proTopic);
+        proPrice = (TextView) findViewById(R.id.proPrice);
+        proSale = (TextView) findViewById(R.id.proSale);
+        proDF = (TextView) findViewById(R.id.proDF);
+        proDT = (TextView) findViewById(R.id.proDT);
+        proDetails = (TextView) findViewById(R.id.proDetails);
+        proImg = (ImageView) findViewById(R.id.proImg);
 
-            }
-        });
+        proTopic.setText(promotionValues.get("promotion").toString());
+        proPrice.setText(promotionValues.get("price").toString()+" Bath");
+        proPrice.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        proSale.setText(promotionValues.get("sale").toString()+" Bath");
+        proDF.setText("Date from : "+promotionValues.get("dateFrom").toString());
+        proDT.setText("Date to     : "+promotionValues.get("dateTo").toString());
+        proDetails.setText(promotionValues.get("details").toString());
+
+        Picasso.with(this).load(promotionValues.get("image").toString()).into(proImg);
+
+        findViewById(R.id.btn_editpromotion).setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_editpromotion:
+
+                Intent cPro = new Intent(PromotionDetails.this,EditPromotion.class);
+                cPro.putExtra("promotion",  promotionValues);
+                startActivity(cPro);
+
+                break;
+
+        }
 
     }
 }
