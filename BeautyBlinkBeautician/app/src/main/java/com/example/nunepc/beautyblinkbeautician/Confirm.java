8010ab139f035d722393ee090182ab02c6ee3307
Unknown <chanasit.captain@hotmail.com>
2017-02-28 15:40:51 +0700

    Verified Bar

--- BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/Confirm.java
+++ BeautyBlinkBeautician/app/src/main/java/com/example/nunepc/beautyblinkbeautician/Confirm.java
@@ -8,6 +8,6 @@ public class Confirm extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm);
+        setContentView(R.layout.activity_offer_confirm);
     }
 }
